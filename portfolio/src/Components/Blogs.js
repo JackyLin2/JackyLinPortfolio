5ae74736c0395119f4108c3ef18f674c38a01df4
JackyLin2 <jacky.jia.lin@gmail.com>
2020-10-04 22:18:12 -0400

    adding blogs

--- portfolio/src/Components/Blogs.js
+++ portfolio/src/Components/Blogs.js
@@ -5,7 +5,9 @@ class Blog extends React.Component{
         return(
             <div>
                 <h1>Blogs: </h1>
-                <a  href="https://medium.com/@jacky.jia.lin"> My Blog Site </a> 
+                <a  href="https://medium.com/@jacky.jia.lin"> My Blog Site </a> <br></br>
+              <a href="https://medium.com/@jacky.jia.lin/what-i-wish-i-knew-before-going-into-flatiron-bootcamp-3ef2a8729eeb">  <img src="https://miro.medium.com/max/770/0*4WVnP8bEzw8W2Ee1"/> </a>
+               <p>What I Wish I Knew Before Going Into Flatiron Bootcamp</p>
                 
             </div>
         )
