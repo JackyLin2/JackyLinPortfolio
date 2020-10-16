0406821cc078804dbf686dd6e0d548a75833e813
JackyLin2 <jacky.jia.lin@gmail.com>
2020-10-16 15:42:58 -0400

    new Blog

--- portfolio/src/Components/Blogs.js
+++ portfolio/src/Components/Blogs.js
@@ -5,9 +5,13 @@ class Blog extends React.Component{
         return(
             <div>
               <h1>Blogs: </h1>
-              <a href="https://medium.com/@jacky.jia.lin/what-i-wish-i-knew-before-going-into-flatiron-bootcamp-3ef2a8729eeb">  <img src="https://miro.medium.com/max/770/0*4WVnP8bEzw8W2Ee1" class="bpicture"  height="100" width="100"/> </a>
+              <a href="https://medium.com/@jacky.jia.lin/what-i-wish-i-knew-before-going-into-flatiron-bootcamp-3ef2a8729eeb">  <img src="https://miro.medium.com/max/770/0*4WVnP8bEzw8W2Ee1" class="bpicture"  height="150" width="150"/> </a>
               <br/>
               <strong>What I Wish I Knew Before Going Into Flatiron Bootcamp</strong>
+              <h1>Blogs: </h1>
+              <a href="https://medium.com/@jacky.jia.lin/what-is-imposter-syndrome-76e7bb694a8b">  <img src="https://miro.medium.com/max/725/0*VGIjaCRbI53mGhxf.jpg" class="bpicture"  height="150" width="150"/> </a>
+              <br/>
+              <strong>What Is Imposter Syndrome?</strong>
             </div>
         )
     }
