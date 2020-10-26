6b1b4e49c8093e798e5cea9233965d3d9590e67f
JackyLin2 <jacky.jia.lin@gmail.com>
2020-10-25 22:23:43 -0400

    projects

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Project extends React.Component{
     render(){
         return(
-            <div>
+            <div className="project">
                 <h1>Projects: </h1>
                 <a  href="https://github.com/JackyLin2"> GitHub </a> 
             </div>
