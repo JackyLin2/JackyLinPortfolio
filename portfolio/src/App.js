e518d8d307bd3bb035c0f5c69064bd072aaef8d2
JackyLin2 <jacky.jia.lin@gmail.com>
2020-10-30 22:43:38 -0400

    bottom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -24,11 +24,11 @@ function App() {
         <Contact/>
         <br/>
         <a href= "https://github.com/JackyLin2" > <AiFillGithub style={{
-        position: 'absolute', left: '47%', top: '330%', fontSize: '30px'}}/> </a> 
+        position: 'absolute', left: '47%', top: '350%', fontSize: '30px'}}/> </a> 
         <a href="https://www.linkedin.com/in/jacky-lin-b6b0b5170/"><AiFillLinkedin style={{
-        position: 'absolute', left: '50%', top: '330%',fontSize: '30px' }}  /> </a> 
+        position: 'absolute', left: '50%', top: '350%',fontSize: '30px' }}  /> </a> 
         <a href="https://medium.com/@jacky.jia.lin"><AiFillMediumSquare  style={{
-        position: 'absolute', left: '52.5%', top: '330%', fontSize:'30px'}}/> </a>
+        position: 'absolute', left: '52.5%', top: '350%', fontSize:'30px'}}/> </a>
         
     </div>
   );
