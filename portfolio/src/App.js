3c5104cf4fd51b51d23ce79bca3d592ae9db8395
JackyLin2 <jacky.jia.lin@gmail.com>
2020-11-15 21:08:01 -0500

    icon fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -17,6 +17,7 @@ function App() {
     <div >
       <NavBar/>
          <h1 className='Profile' style={{ fontSize: '300px' }}> Jacky Lin</h1> 
+         <h1 className='Profile' style={{ fontSize: '100px' }}> View my Work</h1> 
          <img  className='Profile' src={Profile} alt={"Me"} style={{ position: "absolute" }}/>
          <br/>
          <br/>
@@ -29,11 +30,11 @@ function App() {
         <br/>
         <br/>
         <a href= "https://github.com/JackyLin2" > <AiFillGithub style={{
-        position: 'absolute', left: '47%', top: '325%', fontSize: '30px'}}/> </a> 
+        position: 'absolute', left: '47%', top: '450%', fontSize: '30px'}}/> </a> 
         <a href="https://www.linkedin.com/in/jacky-lin-b6b0b5170/"><AiFillLinkedin style={{
-        position: 'absolute', left: '50%', top: '325%',fontSize: '30px' }}  /> </a> 
+        position: 'absolute', left: '50%', top: '450%',fontSize: '30px' }}  /> </a> 
         <a href="https://medium.com/@jacky.jia.lin"><AiFillMediumSquare  style={{
-        position: 'absolute', left: '52.5%', top: '325%', fontSize:'30px'}}/> </a>
+        position: 'absolute', left: '52.5%', top: '450%', fontSize:'30px'}}/> </a>
         
     </div>
   );
