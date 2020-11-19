01cce94b9c937ae0d415ebcc362c127a63e109ae
JackyLin2 <jacky.jia.lin@gmail.com>
2020-11-18 23:00:49 -0500

    blog right

--- portfolio/src/Components/Blogs.js
+++ portfolio/src/Components/Blogs.js
@@ -5,13 +5,20 @@ class Blog extends React.Component{
         return(
             <div className="blogs">
               <h1>Blogs: </h1>
+
+             <div>
               <a href="https://medium.com/@jacky.jia.lin/what-i-wish-i-knew-before-going-into-flatiron-bootcamp-3ef2a8729eeb">  <img src="https://miro.medium.com/max/770/0*4WVnP8bEzw8W2Ee1" class="bpicture"  height="300" width="300"/> </a>
               <br/>
               <strong>What I Wish I Knew Before Going Into Flatiron Bootcamp</strong>
+              </div>
               <br/>
+
+              <div className ="floating-2">
               <a href="https://medium.com/@jacky.jia.lin/what-is-imposter-syndrome-76e7bb694a8b">  <img src="https://miro.medium.com/max/725/0*VGIjaCRbI53mGhxf.jpg" class="bpicture"  height="300" width="300"/> </a>
               <br/>
               <strong>What Is Imposter Syndrome?</strong>
+              </div>
+
               <br/>
               <a href="https://medium.com/@jacky.jia.lin/fizz-buzz-interview-question-6e42e18569ab">  <img src="https://miro.medium.com/max/514/1*UzQ0y9oWOZccYgwosk5ALA.png" class="bpicture"  height="300" width="300"/> </a>
               <br/>
